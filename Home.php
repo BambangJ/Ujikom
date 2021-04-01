20894f5d664a2bb9eebef2b8e5a61757f69efc04
BambangJ <you@example.com>
2021-04-01 11:59:09 +0700

    initislize

---
+++ Home.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+	<title>
+	Qu
+	</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Qu</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+
+		  <form class="container-fluid">
+		<div class="input-group">
+		<input type="text" class="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1">
+		<button class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></button>
+		</div>
+	</form>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+</nav>
+	
